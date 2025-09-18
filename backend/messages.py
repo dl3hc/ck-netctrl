a7b332dab5b0055ddc4d73f1791ebad13a1b0966
dl3hc <krissz2@web.de>
2025-09-18 11:07:15 +0200

    docs: add GPLv3 + Non-Commercial license header to all Python files

--- backend/messages.py
+++ backend/messages.py
@@ -1,3 +1,30 @@
+# -----------------------------------------------------------------------------
+# Christian-Koppler Control Software (ck-netctrl)
+# Copyright (C) 2025 dl3hc
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version, **with the following restriction**:
+#
+# Non-Commercial Use Only:
+# This software may not be used for commercial purposes.
+# Commercial purposes include selling, licensing, or using the software
+# to provide paid services.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details:
+# https://www.gnu.org/licenses/
+#
+# Additional notes:
+# 1. Any modifications or derived works must also be released under
+#    this same license (GPLv3 + Non-Commercial).
+# 2. Redistribution of modified versions must also make the source code
+#    available under this license.
+# -----------------------------------------------------------------------------
+
 def build_messages(val_l: int, val_c: int, highpass: bool):
     """
     Baut alle Nachrichten für den SBC65EC basierend auf L, C und Hoch/Tiefpass.
